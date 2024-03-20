ca7d52af8d8e928f524d2e60b9397d2c244d32c0
El Idrissi Mustapha <appsbymuss@gmail.com>
2024-03-20 15:43:15 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str)
+     if not isinstance(roman_string, str):
         return 0
-    total = 0
-    num = 0
-    digits = { 'I': 1, 'V': 5, 'X': 10 , 'L':50 , 'C': 100 , 'D': 500, 'M': 1000}
-    for r in reversed(roman_string):
-        num = digits[r]
-        total += num if total < num * 5 else -num
-    return total
+    roman_numeral_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    result = 0
+    for i in range(len(roman_string)):
+        current_value = roman_numeral_map[roman_string[i]]
+        if i > 0 and current_value > roman_numeral_map[roman_string[i - 1]]:
+            result += current_value - 2 * roman_numeral_map[roman_string[i - 1]]
+        else:
+            result += current_value
+
+    return result
