d344fa69ee1ff620fa4058ef009cb5e743b04966
El Idrissi Mustapha <appsbymuss@gmail.com>
2024-04-16 16:27:18 -0700

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,29 +1,41 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""
+9. Student to JSON
+"""
 
 
 class Student:
-    """Represent a student."""
+    """class Student that defines a student"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+        """
+        Initialize the object with the provided arguments.
+
         Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+            first_name (str): student's first name.
+            last_name (str): student's last name.
+            age (int): the age
+
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-        Args:
-            attrs (list): (Optional) The attributes to represent.
         """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+        get the dictionary description
+        with simple data structure
+
+        Args:
+            attrs (list): list of attributes
+        """
+        my_dict = {}
+        if attrs is not None:
+            for attr in attrs:
+                try:
+                    my_dict[attr] = self.__dict__[attr]
+                except Exception:
+                    pass
+        else:
+            return self.__dict__
+        return my_dict
